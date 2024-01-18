15fce122f2d7a76db77cfc6d986fcef1c83730c7
zasx1994 <310878176@qq.com>
2024-01-18 14:28:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# 导入依赖库
+from openai import OpenAI
+from dotenv import load_dotenv, find_dotenv
 
+# 加载 .env 文件中定义的环境变量
+_ = load_dotenv(find_dotenv())
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 初始化 OpenAI 客户端
+client = OpenAI()  # 默认使用环境变量中的 OPENAI_API_KEY 和 OPENAI_BASE_URL
